444708a2dfa092194cd5bc5ac7b3b3bc11613bf7
JJJAccii <112672379+JJJAccii@users.noreply.github.com>
2023-03-05 14:16:19 -0800

    Update C# goods class.cs

--- C# goods class.cs
+++ C# goods class.cs
@@ -1,40 +1,101 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 
-namespace goods_with_5_different_props
+namespace Goods
 {
     class GoodsItem
     {
-        public int ProductNum;
-        public string Name;
-        public double Price;
-        public string Picture;
+        public int Num;
+        public double price;
         public int Ordernum;
+
+        public virtual void GetItem(int ProductNum, double Productprice, int Productordernum)
+        {
+            Num = ProductNum;
+            price = Productprice;
+            Ordernum = Productordernum;
+        }
+
+
+     class apple:GoodsItem
+        {
+            public override void GetItem(int ProductNum, double Productprice, int Productordernum)
+            {
+                base.GetItem(ProductNum, Productprice, Productordernum);
+                Console.WriteLine("This item is Apple with product number:" + Num + " ,product price: $" + price + " , and product order number:" + Ordernum);
+            }
+        }
+
+        class phone : GoodsItem
+        {
+            public override void GetItem(int ProductNum, double Productprice, int Productordernum)
+            {
+                base.GetItem(ProductNum, Productprice, Productordernum);
+                Console.WriteLine("This item is Apple with product number:" + Num + " ,product price: $" + price + " , and product order number:" + Ordernum);
+            }
+        }
+ 
+        
+        class PC : GoodsItem
+        {
+            public override void GetItem(int ProductNum, double Productprice, int Productordernum)
+            {
+                base.GetItem(ProductNum, Productprice, Productordernum);
+                Console.WriteLine("This item is Apple with product number:" + Num + " ,product price: $" + price + " , and product order number:" + Ordernum);
+            }
+        }
+
+
+        class Car : GoodsItem
+        {
+            public override void GetItem(int ProductNum, double Productprice, int Productordernum)
+            {
+                base.GetItem(ProductNum, Productprice, Productordernum);
+                Console.WriteLine("This item is Apple with product number:" + Num + " ,product price: $" + price + " , and product order number:" + Ordernum);
+            }
+        }
+
+
+
 
         static void Main(string[] args)
         {
-            GoodsItem one = new GoodsItem { ProductNum = 1, Name = "apple", Price = 1.5, Picture = "apple.jpg", Ordernum = 1 };
-            GoodsItem two = new GoodsItem { ProductNum = 2, Name = "phone", Price = 850, Picture = "phone.jpg", Ordernum = 2 };
-            GoodsItem three = new GoodsItem { ProductNum = 3, Name = "pen", Price = 2, Picture = "pen.jpg", Ordernum = 3 };
-            GoodsItem four = new GoodsItem { ProductNum = 4, Name = "light", Price = 4, Picture = "light.jpg", Ordernum = 4 };
-            GoodsItem five = new GoodsItem { ProductNum = 5, Name = "bmw", Price = 45000, Picture = "bmw.jpg", Ordernum = 5 };
-            GoodsItem six = new GoodsItem { ProductNum = 6, Name = "monster", Price = 2.5, Picture = "monster.jpg", Ordernum = 6 };
-            GoodsItem seven = new GoodsItem { ProductNum = 7, Name = "macbook", Price = 1999, Picture = "macbook.jpg", Ordernum = 7 };
-            GoodsItem eight = new GoodsItem { ProductNum = 8, Name = "boots", Price = 249, Picture = "boots.jpg", Ordernum = 8 };
-            GoodsItem nine = new GoodsItem { ProductNum = 9, Name = "cable", Price = 1.5, Picture = "cable.jpg", Ordernum = 9 };
-            GoodsItem ten = new GoodsItem { ProductNum = 10, Name = "mouse", Price = 85, Picture = "mouser.jpg", Ordernum = 10 };
-         
-        }
-    }
+            GoodsItem myapple = new apple();
+            GoodsItem myphone = new phone();
+            GoodsItem mypc = new PC();
+            GoodsItem mycar = new Car();
+            myapple.GetItem(25, 1.25, 4576);
+            myphone.GetItem(13, 999, 45411);
+            mypc.GetItem(102, 1299, 456478);
+            mycar.GetItem(878, 49999, 471211);
 
-    public GoodsItem itemGet(string name)
-    {
-        foreach (GoodsItem item in items)
-        {
-            if (item.Name == name)
-            {
-                return item;
+
+            List<int> mySet = new List<int>();
+            try
+              {
+                itemSet(mySet, myapple.Ordernum);
+                itemSet(mySet, myapple.Ordernum);
+                itemSet(mySet, myapple.Ordernum);
+                itemSet(mySet, myapple.Ordernum);
             }
+             catch (ArgumentException ex)
+              {
+                    Console.WriteLine(ex.Message);
+              }
+
+
+             static void itemSet(List<int> mySet, int newItem)
+            {
+                if (mySet.Contains(newItem))
+                {
+                    throw new ArgumentException("Item already exists in the set.");
+                }
+                else
+                {
+                    mySet.Add(newItem);
+                }
+            }
+
         }
-        throw new ArgumentException("Item not found.");
     }
 }
